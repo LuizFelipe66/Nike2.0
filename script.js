0818d42c08fa10c1de0da9780692e9162d03964f
LuizFelipe66 <101068627+LuizFelipe66@users.noreply.github.com>
2022-09-05 19:14:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showMenu() {
+  document.getElementById('menuMobile').style.opacity = '1'
+  document.getElementById('menuMobile').style.visibility = 'visible'
+}
+
+function closeMenu() {
+  document.getElementById('menuMobile').style.opacity = '0'
+  document.getElementById('menuMobile').style.visibility = 'hidden'
+}
